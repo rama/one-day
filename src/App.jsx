9f5d04bb890da11606099784961f34dc8d695520
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2024-03-04 21:22:30 -0500

    Add basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [done, setDone] = useState(false);
+	const [isSetupComplete, setIsSetupComplete] = useState(false);
+	const [isDone, setIsDone] = useState(false);
+	const [habit, setHabit] = useState("");
 
 	return (
 		<>
-			<h1>One and Done Habit Tracker</h1>
-			<h2>Did you brush today?</h2>
-
-			{done ? (
-				<p>Woohoo! Come back tomorrow.</p>
+			<h1 className="text-gradient">One Day Habit Tracker</h1>
+			{isSetupComplete ? (
+				<>
+					<h2>
+						Did you <span className="text-gradient">{habit}</span>{" "}
+						today?
+					</h2>
+					{isDone ? (
+						<p>Woohoo! Come back tomorrow.</p>
+					) : (
+						<button onClick={() => setIsDone(true)}>Yes</button>
+					)}
+				</>
 			) : (
-				<button onClick={() => setDone(() => true)}>Yes</button>
+				<form onSubmit={() => setIsSetupComplete(true)}>
+					<h2>
+						I want to{" "}
+						<input
+							type="text"
+							value={habit}
+							onChange={(e) => setHabit(e.target.value)}
+						/>{" "}
+						everyday.
+					</h2>
+					<button>Start</button>
+				</form>
 			)}
 		</>
 	);
